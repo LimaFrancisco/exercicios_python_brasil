e9d8bf5652bf506ce10acddc8cb1aa889f121cf8
Francisco Lima <francisco1111.fl@protonmail.com>
2025-01-28 12:21:22 -0300

    ex004 of sequential structure

---
+++ 01 - EstruturaSequencial/ex004.py
@@ -0,0 +1,8 @@
+note1 = float(input('Enter a first note: '))
+note2 = float(input('Enter a second note: '))
+note3 = float(input('Enter a third note: '))
+note4 = float(input('Enter a fourth note: '))
+
+average = (note1 + note2 + note3 + note4) / 4
+
+print('The average of the notes is ', average)
