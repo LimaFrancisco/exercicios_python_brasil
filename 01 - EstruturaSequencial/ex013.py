3586047c9509aa0355102ba9c4592964dda1b20d
Francisco Lima <francisco1111.fl@protonmail.com>
2025-01-28 15:09:28 -0300

    ex013 of sequential structure

---
+++ 01 - EstruturaSequencial/ex013.py
@@ -0,0 +1,8 @@
+height = float(input('Enter a height: '))
+
+man = (72.7 * height) - 58
+woman = (62.1 * height) - 44.7
+
+print('*' * 10, 'IDEAL WEIGHT', '*' * 10)
+print('man = ', man, 'kg')
+print('woman = ', woman, 'kg')
