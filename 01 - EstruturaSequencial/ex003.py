672d292aad230d2a23efeaef9217b4b0bc38b982
Francisco Lima <francisco1111.fl@protonmail.com>
2025-01-28 12:09:27 -0300

    ex003 of sequential structure

---
+++ 01 - EstruturaSequencial/ex003.py
@@ -0,0 +1,6 @@
+first_number = int(input('Enter a number: '))
+second_number = int(input('Enter another number: '))
+
+numbers_sum = first_number + second_number
+
+print('The sum between ', first_number, ' and ', second_number, ' is ' ,numbers_sum)
