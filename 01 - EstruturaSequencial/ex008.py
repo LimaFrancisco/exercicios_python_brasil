383eee68796ae9148be4e6036906b851afd44c9a
Francisco Lima <francisco1111.fl@protonmail.com>
2025-01-28 14:25:02 -0300

    ex008 of sequential structure

---
+++ 01 - EstruturaSequencial/ex008.py
@@ -0,0 +1,6 @@
+value_per_hour = float(input('How much do you earn per hour? '))
+hours_worked = float(input('How many hours worked? '))
+
+salary = value_per_hour * hours_worked
+
+print('Your salary is: R$ ', salary)
