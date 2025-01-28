b46299ab6901b28b6cdfb5d27c300aac4c3c140d
Francisco Lima <francisco1111.fl@protonmail.com>
2025-01-28 14:52:14 -0300

    ex011 of sequential structure

---
+++ 01 - EstruturaSequencial/ex011.py
@@ -0,0 +1,11 @@
+num1 = int(input('Enter a integer number: '))
+num2 = int(input('Enter another integer number: '))
+num3 = float(input('Enter a real number: '))
+
+solution1 = (num1 * 2) * (num2 / 2)
+solution2 = (num1 * 3) + num3
+solution3 = num3 ** 3
+
+print('The product of twice the first and half the second: ', solution1)
+print('The sum of three times the first and third: ', solution2)
+print('The third raised to the cube: ', solution3)
