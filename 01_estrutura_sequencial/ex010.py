b317d2a30d78a341f5b3320d130fe49ea66d9bf8
LimaFrancisco <francisco1111.fl@protonmail.com>
2025-12-27 12:53:44 -0300

    ex010.py

---
+++ 01_estrutura_sequencial/ex010.py
@@ -0,0 +1,5 @@
+# Faça um programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+celsius = float(input("Infome uma temperatura em graus Celsius: "))
+fahrenheit = (celsius * 9 / 5) + 32
+
+print(f"Convertendo para Fahrenheit fica: {fahrenheit}")
