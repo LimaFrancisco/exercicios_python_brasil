e8864c9a5b7d1e6a84a8fa36704e4be3615eaaac
LimaFrancisco <francisco1111.fl@protonmail.com>
2026-01-03 14:15:57 -0300

    ex012

---
+++ ex012.py
@@ -0,0 +1,6 @@
+#  Tendo como dados de entrada um arquivo em Gigabytes, construa um algoritmo que faça a conversão para Megabytes, usando a seguinte fórmula: Gigabytes * 1024
+
+gigabytes = input("Insira o tamanho do arquivo em gigabytes: ")
+megabytes = gigabytes * 1024
+
+print(f"O valor inserido convertido para megabytes: {megabytes}")
